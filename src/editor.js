b29a59339a63b56a5ae4b390309b8f620a2ffcff
Joel Brady <jbrady@cse.unsw.edu.au>
2013-05-25 16:34:42 +1000

    added clouds and spawn points, almost all working

--- src/editor.js
+++ src/editor.js
@@ -5,13 +5,14 @@ var GRID_LINE_WIDTH = 1;
 var WATER = "#0000FF";
 var SKY = "#bef"
 var WALL = "#ADA96E";
+var CLOUD = "#CCCCCC";
 
-var solid_colors = ["water", "wall", "sky"];
-var color_lookup = [WATER, WALL, SKY];
+var solid_colors = ["water", "wall", "sky", "cloud"];
+var color_lookup = [WATER, WALL, SKY, CLOUD];
 
 // globals
-var context;
 var canvas;
+var context;
 var vertical_cell_size;
 var horizontal_cell_size;
 var mouse_down = false;
@@ -19,41 +20,25 @@ var current_tile;
 var grid;
 var water_level = 4;    //default is 4, entire block is water
 
+var p1_spawn = { row: 0, col: 0 };
+var p2_spawn = { row: 0, col: 0 };
+
 $(window).load(main);
 
 function main() {
     init();
     register_callbacks();
+    load_level(initial_level);
+    dump_level();
     draw_grid();
     draw_grid(); // makes lines thicker!
     draw_grid(); // makes lines thicker!
     draw_grid(); // makes lines thicker!
-    load_level_from_str(initial_level);
 }
 
 function init() {
-    NUM_COLS = $("#map_width").val();
-    NUM_ROWS = $("#map_height").val();
-    
-    $("#io")[0].rows = NUM_ROWS;
-    $("#io")[0].cols = NUM_COLS;
-
-    canvas = $("#editor_canvas")[0]
+    canvas = $("#editor_canvas")[0];
     context = canvas.getContext("2d");
-
-    set_current_tile("wall");
-    vertical_cell_size = (canvas.height - GRID_LINE_WIDTH * NUM_ROWS - 1) / NUM_ROWS;
-    horizontal_cell_size = (canvas.width - GRID_LINE_WIDTH * NUM_COLS - 1) / NUM_COLS;
-    
-    grid = new Array(NUM_ROWS);
-    
-    for (var row = 0; row < NUM_ROWS; ++row) {
-        grid[row] = new Array(NUM_COLS);
-        for (var col = 0; col < NUM_COLS; ++col) {
-            set_current_tile("wall");
-            fill_cell(col, row);
-        }
-    }
 }
 
 function register_callbacks() {
@@ -80,6 +65,9 @@ function register_callbacks() {
     $("#water_button").click(function () { set_current_tile("water"); });
     $("#wall_button").click(function () { set_current_tile("wall"); });
     $("#sky_button").click(function () { set_current_tile("sky"); });
+    $("#cloud_button").click(function () { set_current_tile("cloud"); });
+    $("#player1_spawn_button").click(function () { set_current_tile("p1_spawn"); });
+    $("#player2_spawn_button").click(function () { set_current_tile("p2_spawn"); });
 
     $("#water_level").change(function(e) {
         water_level = e.target.value;
@@ -120,6 +108,11 @@ function draw_grid() {
     for (var j = 1; j < NUM_COLS; ++j) {
         context.fillRect(j * (horizontal_cell_size + GRID_LINE_WIDTH), 0, GRID_LINE_WIDTH, canvas.height);
     }
+
+    context.font = horizontal_cell_size.toString() + "px Arial";
+    context.fillText("1", p1_spawn.x * (horizontal_cell_size + GRID_LINE_WIDTH), p1_spawn.y);
+    context.fillText("2", p2_spawn.x * (horizontal_cell_size + GRID_LINE_WIDTH), p2_spawn.y);
+
     context.fillStyle = oldStyle;
 }
 
@@ -131,14 +124,21 @@ function fill_cell(x, y) {
         var y_start = y * (vertical_cell_size + GRID_LINE_WIDTH)+1;
         if (current_tile == "water") {
             height *= (water_level / 4);
-            //y_start += (vertical_cell_size * (4-water_level)/4);
         }
         context.fillRect(x * (horizontal_cell_size + GRID_LINE_WIDTH)+1, 
                          y_start, horizontal_cell_size, height);
-    } else {
-        console.log("o shit this is not good");
     }
-    grid[y][x] = current_tile != "water" ? current_tile : water_level;
+    if (current_tile != "p1_spawn" && current_tile != "p2_spawn") {
+        grid[y][x] = current_tile != "water" ? current_tile : water_level;
+    }
+    if (current_tile == "p1_spawn") {
+        p1_spawn.row = y;
+        p1_spawn.col = x;
+    }
+    if (current_tile == "p2_spawn") {
+        p2_spawn.row = y;
+        p2_spawn.col = x;
+    }
     draw_grid();
 }
 
@@ -150,11 +150,10 @@ function draw(e) {
 }
 
 function pos_to_coord(e) {
-    // e.offset(X|Y)
-    var ret = new Object();
-    ret.x = Math.floor(e.offsetX / (horizontal_cell_size + GRID_LINE_WIDTH));
-    ret.y = Math.floor(e.offsetY / (vertical_cell_size + GRID_LINE_WIDTH));
-    return ret;
+    return {
+        x: Math.floor(e.offsetX / (horizontal_cell_size + GRID_LINE_WIDTH)),
+        y: Math.floor(e.offsetY / (vertical_cell_size + GRID_LINE_WIDTH))
+    };
 }
 
 function fill_entire_grid() {
@@ -167,6 +166,7 @@ function fill_entire_grid() {
 
 function dump_level() {
     var level = "";
+
     for (var row = 0; row < NUM_ROWS; ++row) {
         for (var col = 0; col < NUM_COLS; ++col) {
             var tile = grid[row][col];
@@ -177,6 +177,9 @@ function dump_level() {
                 break;
                 case "sky":
                     char = " ";
+                break;
+                case "cloud":
+                    char = "c";
                 break;
                 default:
                     if (tile >= 0 && tile <= 4) {
@@ -190,16 +193,54 @@ function dump_level() {
         }
         level += "\n";
     }
-    $("#io").val(level);
+
+    var jsonlevel = {
+        rows: NUM_ROWS,
+        cols: NUM_COLS,
+        p1: p1_spawn,
+        p2: p2_spawn,
+        level_data: level
+    };
+
+    $("#io").val(JSON.stringify(jsonlevel));
 }
 
 function load_level() {
     var level = $("#io").val();
-    loadl_level_from_str(level);
+    load_level_from_str(level);
 }
 
-function load_level_from_str(level) {
+function load_level_from_str(input_json) {
+
+    var jsonlevel = JSON.parse(input_json);
+    load_level(jsonlevel);
+}
+
+function load_level(jsonlevel) {
+    NUM_ROWS = jsonlevel.rows;
+    NUM_COLS = jsonlevel.cols;
+    p1_spawn = jsonlevel.p1;
+    p2_spawn = jsonlevel.p2;
+    var level = jsonlevel.level_data;
+    
+    $("#io")[0].rows = NUM_ROWS;
+    $("#io")[0].cols = NUM_COLS;
+
+    vertical_cell_size = (canvas.height - GRID_LINE_WIDTH * NUM_ROWS - 1) / NUM_ROWS;
+    horizontal_cell_size = (canvas.width - GRID_LINE_WIDTH * NUM_COLS - 1) / NUM_COLS;
+    
+    grid = new Array(NUM_ROWS);
+    
+    set_current_tile("wall");
+    for (var row = 0; row < NUM_ROWS; ++row) {
+        grid[row] = new Array(NUM_COLS);
+        for (var col = 0; col < NUM_COLS; ++col) {
+            fill_cell(col, row);
+        }
+    }
+
     var i = 0;
+
     for (var row = 0; row < NUM_ROWS; ++row) {
         for (var col = 0; col < NUM_COLS; ++col) {
             var tile = level[i];
@@ -214,6 +255,9 @@ function load_level_from_str(level) {
                 break;
                 case ".":
                     entry = "wall";
+                break;
+                case "c":
+                    entry = "cloud";
                 break;
                 default:
                     // assume water
@@ -232,7 +276,11 @@ function load_level_from_str(level) {
     }
 }
 
-var initial_level = "\
+var initial_level = { rows: 10,
+cols: 20,
+p1: {row: 7, col: 2},
+p2: {row: 7, col: 3},
+level_data: "\
 ....................\n\
 .                  .\n\
 .                  .\n\
@@ -242,5 +290,5 @@ var initial_level = "\
 .                  .\n\
 .                  .\n\
 .......34234234.....\n\
-....................\n\
-"
+....................\n"
+};
